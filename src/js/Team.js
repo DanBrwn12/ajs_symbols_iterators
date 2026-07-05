2545bdaa7aa6736a3f2e197f4047849bd249ed5b
PC <daway4554@gmail.com>
2026-07-05 11:35:53 +0300

    Исправление недостатков

--- src/js/Team.js
+++ src/js/Team.js
@@ -4,6 +4,16 @@ export default class Team {
     }
 
     [Symbol.iterator]() {
-        return this.characters[Symbol.iterator]()
+        let index = 0;
+        const characters = this.characters;
+
+        return {
+            next() {
+                if (index < characters.length) {
+                    return {value: characters[index++], done: false}
+                }
+                return {done: true}
+            }
+        }
     }
 }
